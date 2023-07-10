be7691f971954be19424cad2b416cfbd16be6c65
unknown <skawldhks@naver.com>
2023-07-10 13:05:15 +0900

    230710_3rd

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,10 +1,12 @@
 import CarouselMain from "./components/CarouselMain";
 import Layout from "./components/Layout";
+import MultiCarousel from "./components/MultiCarousel";
 import TitleTwoRow from "./components/TitleTwoRow";
 import AcademyAsset2 from "./components/asset/AcademyAseet2";
 import AcademyAsset from "./components/asset/AcademyAsset";
 import AcademyAsset3 from "./components/asset/AcademyAsset3";
 import AcademyAsset4 from "./components/asset/AcademyAsset4";
+import KakaotalkAsset from "./components/asset/KakaotalkAsset";
 
 export default function Page() {
     return (
@@ -13,7 +15,7 @@ export default function Page() {
                 {/* 메인캐러셀 이미지 */}
                 <CarouselMain />
             </div>
-            {/* 그리드 3/5 2/5 구분 */}
+            {/* 1번째 그리드 3/5 2/5 구분 */}
             <div className="py-16 w-full">
                 <div className="w-full px-4">
                     <div className="grid grid-cols-5 gap-16">
@@ -110,6 +112,119 @@ export default function Page() {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            {/* 2번째 그리드 3/5 2/5 구분 */}
+            <div className="py-16 w-full">
+                <div className="w-full px-4">
+                    <div className="grid grid-cols-5 gap-16">
+                        {/* 소셜네트워크 */}
+                        <div className="col-span-3">
+                            <TitleTwoRow
+                                main="소셜네트워크"
+                                sub="social network"
+                            />
+                            <div className="w-full grid grid-cols-2 gap-4">
+                                {/* 1번째 자식 */}
+                                <div className="w-full cursor-pointer aspect-video relative">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1562329265-95a6d7a83440?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1070&q=80"
+                                        alt="social image1"
+                                    />
+                                    <p className="font-semibold py-3 px-1">
+                                        오페라테 Operatte
+                                    </p>
+                                    {/* 호버했을 때 음영처리용 빈박스 */}
+                                    <div className="absolute top-0 left-0 w-full h-full bg-[hsl(0,0%,98.4%,0.2)] opacity-0 transition-all duration-300 ease-in-out hover:opacity-100"></div>
+                                    {/* 오른쪽 위 아이콘 */}
+                                    <div className="absolute top-0 right-0 w-12 h-12 bg-white/70 flex justify-center items-center">
+                                        <KakaotalkAsset />
+                                    </div>
+                                </div>
+                                {/* 2번째 자식 */}
+                                <div className="w-full cursor-pointer aspect-video relative">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1516307365426-bea591f05011?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1459&q=80"
+                                        alt="social image1"
+                                    />
+                                    <p className="font-semibold py-3 px-1">
+                                        카페라테 caffeelatte
+                                    </p>
+                                    {/* 호버했을 때 음영처리용 빈박스 */}
+                                    <div className="absolute top-0 left-0 w-full h-full bg-[hsl(0,0%,98.4%,0.2)] opacity-0 transition-all duration-300 ease-in-out hover:opacity-100"></div>
+                                    {/* 오른쪽 위 아이콘 */}
+                                    <div className="absolute top-0 right-0 w-12 h-12 bg-white/70 flex justify-center items-center">
+                                        <KakaotalkAsset />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* 알림존 */}
+                        <div className="col-span-2 flex flex-col space-y-8">
+                            <TitleTwoRow main="알림존" sub="doh banner" />
+                            <MultiCarousel>
+                                {/* 첫번째 이미지 */}
+                                <div className="w-52 h-60 bg-gray-400">
+                                    <img
+                                        className="w-full h-full object-cover object-center"
+                                        src="https://images.unsplash.com/photo-1688448543484-ecd531cba9bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+                                        alt="alimzone1"
+                                    />
+                                </div>
+                                {/* 두번쨰 이미지 */}
+                                <div className="w-52 h-60 bg-gray-400">
+                                    <img
+                                        className="w-full h-full object-cover object-center"
+                                        src="https://images.unsplash.com/photo-1669940812749-0a0fa4b92ba4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+                                        alt="alimzone1"
+                                    />
+                                </div>
+                                {/* 세번쨰 이미지 */}
+                                <div className="w-52 h-60 bg-gray-400">
+                                    <img
+                                        className="w-full h-full object-cover object-center"
+                                        src="https://plus.unsplash.com/premium_photo-1675756583871-6be3905c4ef4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+                                        alt="alimzone1"
+                                    />
+                                </div>
+                                {/* 네번쨰 이미지 */}
+                                <div className="w-52 h-60 bg-gray-400">
+                                    <img
+                                        className="w-full h-full object-cover object-center"
+                                        src="https://images.unsplash.com/photo-1682687221073-53ad74c2cad7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+                                        alt="alimzone1"
+                                    />
+                                </div>
+                            </MultiCarousel>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* 유관기관 */}
+            <div className="w-full px-4">
+                <div className="grid grid-cols-5 gap-16 items-end">
+                    {/* 타이틀 */}
+                    <div>
+                        <TitleTwoRow main="유관기관" sub="family sites" />
+                    </div>
+                    {/* 캐러셀 이미지 */}
+                    <div className=" col-span-4">
+                        <MultiCarousel itemNum={5}>
+                            {Array(9)
+                                .fill("")
+                                .map((_, i) => (
+                                    <div key={i}>
+                                        <img
+                                            src={`https://www.daeguoperahouse.org/images/main/img_ban0${
+                                                i + 1
+                                            }_on.png`}
+                                            alt="image family site"
+                                        />
+                                    </div>
+                                ))}
+                        </MultiCarousel>
                     </div>
                 </div>
             </div>

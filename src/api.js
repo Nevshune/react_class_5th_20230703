eff8d09902b0a16939708f1a7321b80afd4ea561
unknown <skawldhks@naver.com>
2023-08-07 13:01:58 +0900

    20230807 first

--- src/api.js
+++ src/api.js
@@ -1,4 +1,7 @@
-const BASE_PATH = "http://localhost:8080";
+const BASE_PATH =
+    process.env.NODE_ENV === "production"
+        ? "https://port-0-reactproject-5-db-eu1k2lll07brk9.sel4.cloudtype.app/"
+        : "http://localhost:8080";
 
 export async function rentalNotices() {
     return await fetch(`${BASE_PATH}/api/rental/notice`, {
